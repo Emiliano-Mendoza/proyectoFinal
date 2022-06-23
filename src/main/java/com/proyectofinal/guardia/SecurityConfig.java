324daca35036a5d11c0d3829b21032d36a86fbc8
Emiliano Mendoza <eemmiii96@gmail.com>
2022-06-23 18:36:48 -0300

    primer incremento terminado

--- src/main/java/com/proyectofinal/guardia/SecurityConfig.java
+++ src/main/java/com/proyectofinal/guardia/SecurityConfig.java
@@ -37,7 +37,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/audios/**", "/images/**", "/js/**", "/webfonts/**").permitAll()		
+		http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/audios/**", "/images/**", "/js/**", "/webfonts/**").permitAll()
+		.antMatchers("/views/usuario/administrar").hasAnyRole("ADMIN")
+		.antMatchers("/views/usuario/crear").hasAnyRole("ADMIN")
+		.antMatchers("/views/usuario/editar").hasAnyRole("ADMIN")
+		.antMatchers("/views/empleado/administrar").hasAnyRole("ADMIN")
+		.antMatchers("/views/empleado/crear").hasAnyRole("ADMIN")
+		.antMatchers("/views/empleado/editar").hasAnyRole("ADMIN")
+		.antMatchers("/views/proveedor/administrar").hasAnyRole("ADMIN")
+		.antMatchers("/views/proveedor/crear").hasAnyRole("ADMIN")
+		.antMatchers("/views/proveedor/editar").hasAnyRole("ADMIN")
+		.antMatchers("/views/vehiculo/administrar").hasAnyRole("ADMIN")
+		.antMatchers("/views/vehiculo/crear").hasAnyRole("ADMIN")
+		.antMatchers("/views/vehiculo/editar").hasAnyRole("ADMIN")
+		.antMatchers("/views/ingresos-empleado").hasAnyRole("GUARDIA","ADMIN")
+		.antMatchers("/views/ingresos-empleado/**").hasAnyRole("GUARDIA","ADMIN")
+		.antMatchers("/views/ingresos-proveedor").hasAnyRole("GUARDIA","ADMIN")
+		.antMatchers("/views/ingresos-proveedor/**").hasAnyRole("GUARDIA","ADMIN")
+		.antMatchers("/views/ronda").hasAnyRole("GUARDIA","ADMIN")
+		.antMatchers("/views/ronda/**").hasAnyRole("GUARDIA","ADMIN")
+		
+		.antMatchers("/views/retiro-material/autorizaciones").hasAnyRole("GUARDIA","ADMIN")
+		.antMatchers("/views/retiro-material/retiro/**").hasAnyRole("GUARDIA","ADMIN")
+		.antMatchers("/views/retiro-material/nueva-autorizacion").hasAnyRole("AUTORIZADOR","ADMIN")
+		.antMatchers("/views/retiro-material/nueva-autorizacion/**").hasAnyRole("AUTORIZADOR","ADMIN")
+		.antMatchers("/views/retiro-material/autorizaciones/**").hasAnyRole("AUTORIZADOR","GUARDIA","ADMIN")
+		
+		.antMatchers("/views/sector-trabajo/administrar").hasAnyRole("ADMIN")
+		.antMatchers("/views/material/administrar").hasAnyRole("ADMIN")
+		
 		.anyRequest().authenticated()
 		.and()
 		.formLogin().loginPage("/login").permitAll()
