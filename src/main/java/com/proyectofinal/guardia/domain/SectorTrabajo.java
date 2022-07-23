da833318c7eb46619707ba3d3dd512a7b7f38c99
Emiliano Mendoza <eemmiii96@gmail.com>
2022-07-22 22:50:18 -0300

    Signed-off-by: Emiliano Mendoza <eemmiii96@gmail.com>

--- src/main/java/com/proyectofinal/guardia/domain/SectorTrabajo.java
+++ src/main/java/com/proyectofinal/guardia/domain/SectorTrabajo.java
@@ -1,6 +1,10 @@
 package com.proyectofinal.guardia.domain;
 
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
